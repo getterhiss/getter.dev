77720fc4c4db5da86b29cde3b1dd7938e6e4dbad
Getter Hiss <getter@hiss.co>
2021-12-20 14:58:43 -0800

    Added Lottie Logo Animation

--- App.tsx
+++ App.tsx
@@ -1,128 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Lottie from 'lottie-react-native';
+import source from './assets/g-icon.json';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import BootSplash from 'react-native-bootsplash';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Splash = () => {
+  const animation = useRef(null);
+  
+  useEffect(()=>{
+    // BootSplash.hide({ fade: true });
+    // return runAnimation();
 
-import RNBootSplash from "react-native-bootsplash";
-import { NavigationContainer } from '@react-navigation/native';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  React.useEffect(() => {
     const timer = setTimeout(() => {
-      RNBootSplash.hide({ fade: true });
-    }, 1000);
+      BootSplash.hide({ fade: true });
+      //@ts-ignore
+      animation?.current?.play();
+    }, 500);
 
     return () => clearTimeout(timer);
-  }, [])
+  }, []);
 
   return (
-    <NavigationContainer>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Lottie
+        ref={animation}
+        style={{width: 300, height: 300}}
+        loop={false}
+        speed={1.8}
+        source={source}
+      />
+    </View>
   );
-};
+}
+export default Splash;
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {
+  flex: 1,
+  flexDirection: 'column',
+    backgroundColor: '#ffc0cb',
+    justifyContent: "center",
+    alignItems: 'center'
+  }
 });
-
-export default App;
