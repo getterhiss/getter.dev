c7310367e01cd2db200a0c7806293849ec51af25
Getter Hiss <getter@hiss.co>
2021-12-23 11:25:10 -0800

    Android: react-native-twilio-video-webrtc

--- android/app/src/main/java/com/getterhiss/MainApplication.java
+++ android/app/src/main/java/com/getterhiss/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.twiliorn.library.TwilioPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new TwilioPackage());
           return packages;
         }
 
