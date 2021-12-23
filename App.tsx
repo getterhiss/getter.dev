5d71b4613db644c064417656c360c86a8eb3e580
Getter Hiss <getter@hiss.co>
2021-12-23 10:46:52 -0800

    SafeAreaProvider w/ transparent StatusBar

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@
  * https://reactnavigation.org/docs/hello-react-navigation
  */
 import * as React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorHandler  from 'components/Error';
@@ -20,22 +22,25 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorHandler}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Splash"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{animation: 'fade'}}
-          />
-          <Stack.Screen name="Video" component={VideoScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar barStyle={'light-content'} translucent={true} backgroundColor="transparent" />
+          <Stack.Navigator
+            initialRouteName="Splash"
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="Splash" component={SplashScreen} />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{animation: 'fade'}}
+            />
+            <Stack.Screen name="Video" component={VideoScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </ErrorBoundary>
   );
 }
